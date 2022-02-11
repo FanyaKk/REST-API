3ede816f5bb3fb7568d654b6df06979f06c36600
FanyaKk <fania.karamfilova@gmail.com>
2022-02-11 20:32:44 +0200

    Books Create/Index/New

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const router = express.Router()
+const Book = require('../models/book')
 
-router.get('/', (req, res) => {
-    res.render('index') //name of index.ejs file in views
+router.get('/', async (req, res) => {
+    let books
+    try {
+        books = await Book.find().sort({ createAt: 'desc'})
+        .limit(10).exec()
+    } catch {
+        books = []
+    }
+    res.render('index', {books: books}) //name of index.ejs file in views
 })
 
 module.exports = router
